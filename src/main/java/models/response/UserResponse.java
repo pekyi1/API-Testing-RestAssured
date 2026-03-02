185919a4569c59a5e8f063bea3d369503a9c3c36
Student <student@example.com>
2026-03-02 08:21:55 +0000

    feat(models): add UserResponse POJO

---
+++ src/main/java/models/response/UserResponse.java
@@ -0,0 +1,44 @@
+package models.response;
+
+/**
+ * POJO representing the response received after creating a User.
+ */
+public class UserResponse {
+
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
