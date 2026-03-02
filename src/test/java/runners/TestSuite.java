b78884585d5058486a7fd18a0968d8772eff1f79
Student <student@example.com>
2026-03-02 08:22:11 +0000

    test(runner): add generic TestSuite for all tests

---
+++ src/test/java/runners/TestSuite.java
@@ -0,0 +1,16 @@
+package runners;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+/**
+ * Executes all test classes across all packages at once.
+ */
+@Suite
+@SuiteDisplayName("All API Tests Suite")
+@SelectPackages("tests")
+public class TestSuite {
+    // This class remains empty. It serves only as a holder for the above
+    // annotations.
+}
